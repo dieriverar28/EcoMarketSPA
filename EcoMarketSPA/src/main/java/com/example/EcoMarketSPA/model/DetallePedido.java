0c02e404e052af9814710e49d8e46eab9d5bb327
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:26:00 -0400

    detalle

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/DetallePedido.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/DetallePedido.java
@@ -1,5 +1,10 @@
 package com.example.EcoMarketSPA.model;
 
 public class DetallePedido {
-    private
+    private int id_detalle;//pk
+    private int id_pedido;//fk
+    private int id_producto;//fk
+    private int cantidad;
+    private int precio_unitario;
+    
 }
