7c8eac7892ef687b72fe88afe6c93fd97d74cbc0
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:05:08 -0400

    tienda

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Tienda.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Tienda.java
@@ -1,9 +1,9 @@
 package com.example.EcoMarketSPA.model;
 
 public class Tienda {
-    private int id_tienda;
+    private int id_tienda; //pk
     private String nombre;
     private String direccion;
-    private int id_comuna;
-    private int id_region;
+    private int id_comuna; //fk comuna
+    private int id_region; //fk region
 }
