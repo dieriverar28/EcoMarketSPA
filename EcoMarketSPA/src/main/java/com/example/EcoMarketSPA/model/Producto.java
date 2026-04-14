da2cf97c8218af5f00512b81594960068a5f1927
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 12:50:35 -0400

    producto

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Producto.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Producto.java
@@ -1,5 +1,10 @@
 package com.example.EcoMarketSPA.model;
 
 public class Producto {
-    private
+    private int id_producto;
+    private String nombre;
+    private String  descripcion;
+    private String categoria;
+    private int precio_base;
+    private boolean estado;
 }
