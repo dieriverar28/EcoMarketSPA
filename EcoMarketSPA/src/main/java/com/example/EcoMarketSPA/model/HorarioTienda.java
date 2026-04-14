0d60b4a0f58f7a43f736c9f87a8d298b6eaed4e3
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:31:48 -0400

    horariotienda

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/HorarioTienda.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/HorarioTienda.java
@@ -1,5 +1,11 @@
 package com.example.EcoMarketSPA.model;
 
+import java.sql.Time;
+
 public class HorarioTienda {
-    private 
+    private int id_horario;//pk
+    private int id_tienda;//fk
+    private String dia_semana;
+    private Time hora_apertura;
+    private Time hora_cierre;
 }
