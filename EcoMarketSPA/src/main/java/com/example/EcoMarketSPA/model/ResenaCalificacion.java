5376dca556cd257253735343627711114b0cd5b4
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:33:25 -0400

    resena

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/ResenaCalificacion.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/ResenaCalificacion.java
@@ -1,5 +1,13 @@
 package com.example.EcoMarketSPA.model;
 
+import java.sql.Date;
+
 public class ResenaCalificacion {
-    private
+    private int id_resena;//pk
+    private int id_cliente;//fk
+    private int id_producto;//fk
+    private String motivo;
+    private boolean estado;
+    private Date fecha;
 }
+
