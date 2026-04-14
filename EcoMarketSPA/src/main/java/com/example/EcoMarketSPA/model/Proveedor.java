0a7253522d529e1c9c6e07f6eff9f25227268cf0
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 12:53:39 -0400

    proveedor

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Proveedor.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Proveedor.java
@@ -1,5 +1,9 @@
 package com.example.EcoMarketSPA.model;
 
 public class Proveedor {
-    private
+    private int id_provedor;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private int id_comuna;
 }
