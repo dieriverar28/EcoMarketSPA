a143e40db0db12b3b971717ebbdf11f60d517568
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:02:26 -0400

    usuario

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Usuario.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Usuario.java
@@ -1,5 +1,10 @@
 package com.example.EcoMarketSPA.model;
 
 public class Usuario {
-    private
+    private int id_usuario;
+    private String nombre;
+    private String email;
+    private int id_rol;//fk rol
+    private int id_tienda;//fk tienda
+    private boolean estado; 
 }
