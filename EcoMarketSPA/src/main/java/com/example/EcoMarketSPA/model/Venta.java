af553109b274c2f77690fc612bbb7537e13aafbb
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:42:13 -0400

    Atributos de venta

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Venta.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Venta.java
@@ -1,5 +1,14 @@
 package com.example.EcoMarketSPA.model;
 
+import java.sql.Date;
+
 public class Venta {
-    private
+    private int id_venta;//pk
+    private int id_pedido;//fk
+    private int id_tienda;//fk
+    private int id_cliente;//FK
+    private Date fecha_venta;
+    private int total_neto;
+    private int descuento_aplicado;
+    private String tipo_documento;
 }
