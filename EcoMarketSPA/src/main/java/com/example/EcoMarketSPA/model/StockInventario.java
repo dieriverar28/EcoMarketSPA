27b0c3087e766327e4c102adafe61393ac23ba07
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:35:39 -0400

    stock

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/StockInventario.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/StockInventario.java
@@ -1,5 +1,9 @@
 package com.example.EcoMarketSPA.model;
 
 public class StockInventario {
-    private
+    private int id_stock;//pk
+    private int id_tienda;//fk
+    private int id_producto;
+    private int cantidad;
+    private int stock_min;
 }
