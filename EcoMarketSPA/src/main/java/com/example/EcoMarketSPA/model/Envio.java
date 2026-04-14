a07447c8d13832572271ee9acc4f14c2e37829e3
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:12:02 -0400

    envio

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Envio.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Envio.java
@@ -1,5 +1,14 @@
 package com.example.EcoMarketSPA.model;
 
+import java.sql.Date;
+
 public class Envio {
-    private
+    private int id_envio; //pk
+    private int id_cliente; //fk
+    private int id_provedor;//fk
+    private int id_ruta; //fk
+    private boolean estado;
+    private Date fecha_despacho;
+    private Date fecha_entrega_est;
+
 }
