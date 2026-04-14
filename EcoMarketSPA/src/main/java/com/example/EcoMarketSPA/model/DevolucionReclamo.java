da859853dd5207190e8405d213e9f5ade0b87857
valerupayan12 <va.rupayan@duocuc.cl>
2026-04-14 13:29:57 -0400

    devolucionReclamo

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/DevolucionReclamo.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/DevolucionReclamo.java
@@ -1,5 +1,13 @@
 package com.example.EcoMarketSPA.model;
 
+import java.sql.Date;
+
 public class DevolucionReclamo {
-    private
+    private int id_devolucion;//pk
+    private int id_venta;//fk
+    private int id_cliente;//fk
+    private int id_producto;//fk
+    private String motivo;
+    private boolean estado;
+    private Date fecha;
 }
