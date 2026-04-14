f89898cb376745247c10cf8e463a32c0f7212cec
dieriverar28 <die.riverar@duocuc.cl>
2026-04-14 12:22:33 -0400

    boleta

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Boleta.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/model/Boleta.java
@@ -1,5 +1,17 @@
 package com.example.EcoMarketSPA.model;
 
 public class Boleta {
-    private
+    private int id_boleta;
+    private int id_venta;
+    private String folio;
+    private String timbre_electronico;
+    private String nombre_receptor;
+    private String rut_receptor;
+    private String fecha_emision;
+    private int monto_neto;
+    private int monto_iva;
+    private int monto_total;
+    private String email_envio;
+    private boolean anulada;
+
 }   
