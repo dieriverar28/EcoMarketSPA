7dc385c83d5b0a0a3415c5031408a956782f386f
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 19:28:08 -0400

    serviceHorarioTienda

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/HorarioTiendaService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/HorarioTiendaService.java
@@ -1,5 +1,36 @@
 package com.example.EcoMarketSPA.service;
 
-public class HorarioTiendaService {
+import java.util.List;
 
-}
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.HorarioTienda;
+import com.example.EcoMarketSPA.repository.HorarioTiendaRepository;
+
+public class HorarioTiendaService {
+    @Autowired
+    //
+    private HorarioTiendaRepository horarioTiendaRepository;
+    //obtenre todos
+    public List<HorarioTienda> getHorarioTienda(){
+        return horarioTiendaRepository.obtenerHorarioTiendas();
+    }
+    //buscar por id 
+    public HorarioTienda getHorarioTienda(int id_horario_tienda){
+        return horarioTiendaRepository.buscarHorarioTienda(id_horario_tienda);
+    }
+    //eliminar por id
+    public int delateHorarioTienda(int id_horario_tienda){
+        return horarioTiendaRepository.eliminarHorario(id_horario_tienda);
+    }
+    //guardadr por id 
+    public HorarioTienda saveHorarioTienda(HorarioTienda horariotienda){
+        return horarioTiendaRepository.guardarHorarioTienda(horariotienda);
+    }
+    //modificar por id 
+    public HorarioTienda updateHorarioTienda(HorarioTienda horarioTienda){
+        return horarioTiendaRepository.modificarHorarioTienda(horarioTienda);
+    }
+
+    }
+
