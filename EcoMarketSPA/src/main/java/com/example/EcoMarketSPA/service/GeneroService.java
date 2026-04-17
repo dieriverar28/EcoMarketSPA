f683777875978c976c62e096c6ff1c8f024434f7
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 18:52:28 -0400

    Update GeneroService.java

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/GeneroService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/GeneroService.java
@@ -1,5 +1,33 @@
 package com.example.EcoMarketSPA.service;
 
 public class GeneroService {
+    @Autowired
+    //llama repository
+    private GeneroService GeneroRepository;
+    //obtener todo
+    public List<Genero> getGenero(){
+        return GeneroRepository.obtenerGenero();
+    }
+    //buscar x id_genero
+    public Genero getGenero(){
+        return GeneroRepository.buscarGenero(id_genero);
+    }
+    //eliminar x id_gemero
+    public int deleteGenero(int_Genero){
+        return GeneroRepository.eliminarGenero(id_genero);
+    }
+    //guardar genero
+    public Genero saveGenero(Genero genero){
+        return GeneroRepository.guardarGenero(genero);
+    }
+    //modificar genero 
+    public int updateGenero(Genero genero){
+        return GeneroRepository.modificarGenero(genero); 
+
+
+    }
+
+
+
 
 }
