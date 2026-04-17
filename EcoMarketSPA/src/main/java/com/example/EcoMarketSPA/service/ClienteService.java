57a0e8380f31c4879bba582d45767b23cca1a7f0
dieriverar28 <die.riverar@duocuc.cl>
2026-04-17 09:12:49 -0400

    clienteservlisto

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ClienteService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ClienteService.java
@@ -1,5 +1,37 @@
 package com.example.EcoMarketSPA.service;
 
-public class ClienteService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.EcoMarketSPA.model.Cliente;
+import com.example.EcoMarketSPA.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+        @Autowired
+//SE LLAMA AL REPOSITORIO PARA PODER USAR SUS FUNCIONES
+    private ClienteRepository clienteRepository; 
+
+//OBTENER clientes
+    public List<Cliente> getAllClientes() {
+        return clienteRepository.obtenerClientes();
+    }
+//OBTENER BOLETA POR ID
+    public Cliente getBoletaById(int id_cliente) {
+        return clienteRepository.buscarCliente(id_cliente);
+    }
+//CREAR cliente
+    public Cliente saveBoletas(Cliente cliente) {
+        return clienteRepository.guardarCliente(cliente);
+    }
+//ACTUALIZAR cliente
+    public int updateCliente(Cliente cliente) {
+        return clienteRepository.modificarCliente(cliente);
+    }
+//ELIMINAR cliente
+    public int deleteCliente(int id_cliente) {
+        return clienteRepository.eliminarCliente(id_cliente);
+    }
 }
