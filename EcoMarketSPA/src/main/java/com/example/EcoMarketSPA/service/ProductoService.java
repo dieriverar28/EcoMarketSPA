ff26a8ccc066f9dd6bb88ee835f2c45b535fb9d0
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 19:58:45 -0400

    serviceProducto

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ProductoService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ProductoService.java
@@ -1,5 +1,34 @@
 package com.example.EcoMarketSPA.service;
 
-public class ProductoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.Producto;
+import com.example.EcoMarketSPA.repository.ProductoRepository;
+
+public class ProductoService {
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    //obtener
+    public List<Producto> getProductos(){
+        return productoRepository.obtenerProductos();
+    }
+    //buscar
+    public Producto getProducto(int id_producto){
+        return productoRepository.buscarProducto(id_producto);
+    }
+    //eliminar
+    public int delateProducto(int id_producto){
+        return productoRepository.eliminarProducto(id_producto);
+    }
+    //guardar
+    public Producto saveProducto(Producto producto){
+        return productoRepository.guardarProducto(producto);
+    }
+    //modifiicar
+    public Producto updateProducto(Producto producto){
+        return productoRepository.modificarProductoProducto(producto);
+    }
 }
