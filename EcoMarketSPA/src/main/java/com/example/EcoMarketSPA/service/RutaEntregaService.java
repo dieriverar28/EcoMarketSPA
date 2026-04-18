b6b52d8385da7a5a5c80de17383b542d10717314
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:31:08 -0400

    serviceRutaEntrega

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RutaEntregaService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RutaEntregaService.java
@@ -1,5 +1,35 @@
 package com.example.EcoMarketSPA.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.RutaEntrega;
+import com.example.EcoMarketSPA.repository.RutaEntregaRepository;
+
 public class RutaEntregaService {
+    @Autowired
+    private RutaEntregaRepository rutaEntregaRepository;
+    //obtener
+    public List<RutaEntrega> getRutaEntregas(){
+        return rutaEntregaRepository.obtenerRutaEntregas();
+    }
+    //bucar
+    public RutaEntrega getRutaEntrega(int id_ruta_entrega){
+        return rutaEntregaRepository.buscarRutaEntrega(id_ruta_entrega);
+    }
+    //eliminar
+    public int deleteRutaEntrega(int id_ruta_entrega){
+        return rutaEntregaRepository.eliminarRutaEntrega(id_ruta_entrega);
+    }
+    //buardar
+    public RutaEntrega saveRutaEntrega(RutaEntrega rutaEntrega){
+        return rutaEntregaRepository.guardarRutaEntrega(rutaEntrega)
+    }
+    //modificar
+    public RutaEntrega updateRutaEntrega(RutaEntrega rutaEntrega){
+        return rutaEntregaRepository.modificarRuta(rutaEntrega)
+    }
+
 
 }
