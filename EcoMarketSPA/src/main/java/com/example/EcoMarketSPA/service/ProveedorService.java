c45337724f183fc48258d0dd090ee6a7a43791cf
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:09:52 -0400

    service proveedor

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ProveedorService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/ProveedorService.java
@@ -1,5 +1,36 @@
 package com.example.EcoMarketSPA.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.Proveedor;
+import com.example.EcoMarketSPA.repository.ProveedorRepository;
+
 public class ProveedorService {
+    @Autowired
+    private ProveedorRepository proveedorRepository;
+
+    //obtener
+    public List<Proveedor> getProveedores(){
+        return proveedorRepository.obtenerProveedores();
+    }
+    //bucar
+    public Proveedor getProveedor(int id_proveedor){
+        return proveedorRepository.buscarProveedor(id_proveedor);
+    }
+    //eliminar
+    public int delateProveedor(int id_proveedor){
+        return proveedorRepository.eliminarProveedor(id_proveedor);
+    }
+    //guardar
+    public Proveedor saveProveedor(Proveedor proveedor){
+        return proveedorRepository.guardarProveedor(proveedor);
+    }
+    //modificar
+    public Proveedor updateProveedor(Proveedor proveedor){
+        return proveedorRepository.modificarProveedor(proveedor);
+    }
+
 
 }
