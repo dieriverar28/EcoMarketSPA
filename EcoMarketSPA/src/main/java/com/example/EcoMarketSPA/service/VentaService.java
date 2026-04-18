a23c3917d87212271809fae5e298ba0aca5cdea0
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:40:32 -0400

    serviceVenta

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/VentaService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/VentaService.java
@@ -1,5 +1,35 @@
 package com.example.EcoMarketSPA.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.Venta;
+import com.example.EcoMarketSPA.repository.VentaRepository;
+
 public class VentaService {
+    @Autowired
+    private VentaRepository ventaRepository;
+    //obtener
+    public List<Venta> getVentas(){
+        return ventaRepository.obtenerVentas();
+    }
+    //bucar
+    public Venta getVenta(int id_venta){
+        return ventaRepository.buscarVenta(id_venta);
+    }
+    //eliminar
+    public int deleteVenta(int id_venta){
+        return ventaRepository.eliminarVenta(id_venta);
+    }
+    //buardar
+    public Venta saveVenta(Venta venta){
+        return ventaRepository.guardarVenta(venta);
+    }
+    //modificar
+    public Venta updateVenta(Venta venta){
+        return ventaRepository.modificarVenta(venta);
+    }
+
 
 }
