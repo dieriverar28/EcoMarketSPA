6f24c7233481cee264cd8171beaf3b5628b4ba5c
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:35:29 -0400

    serviceStockInventario

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/StockInventarioService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/StockInventarioService.java
@@ -1,5 +1,35 @@
 package com.example.EcoMarketSPA.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.StockInventario;
+import com.example.EcoMarketSPA.repository.StockInventarioRepository;
+
 public class StockInventarioService {
+    @Autowired
+    private StockInventarioRepository stockInventarioRepository;
+    //obtener
+    public List<StockInventario> getStockInventarios(){
+        return stockInventarioRepository.obtenerStockInventarios();
+    } 
+    //bucar
+    public StockInventario getStockInventario(int id_stock_inventario){
+        return stockInventarioRepository.buscarStockInventario(id_stock_inventario);
+    }
+    //eliminar
+    public int deleteStockInventario(int id_stock_inventario){
+        return stockInventarioRepository.eliminarStockInventario(id_stock_inventario);
+    }
+    //buardar
+    public StockInventario saveStockInventario(StockInventario stockInventario){
+        return stockInventarioRepository.guardarStockInventario(stockInventario)
+    }
+    //modificar
+    public StockInventario updateStockInventario(StockInventario stockInventario){
+        return stockInventarioRepository.modificarStockInventario(stockInventario);
+    }
+
 
 }
