9f0205599030887cea22696d71341de17ae5a014
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:25:30 -0400

    serviceRolPermiso

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RolPermisoService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RolPermisoService.java
@@ -1,5 +1,36 @@
 package com.example.EcoMarketSPA.service;
 
-public class RolPermisoService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.RolPermiso;
+import com.example.EcoMarketSPA.repository.RolPermisoRepository;
+
+public class RolPermisoService {  
+    @Autowired
+    private RolPermisoRepository rolPermisoRepository;
+
+    //obtener
+    public List<RolPermiso> getRolPermisos(){
+        return rolPermisoRepository.obtenerRolPermiso();
+    }
+    //bucar
+    public RolPermiso getRolPermiso(int id_rol_permiso){
+        return rolPermisoRepository.buscarRolPermiso(id_rol_permiso);
+    }
+    //eliminar
+    public int deleteRolPermiso(int id_rol_permiso){
+        return rolPermisoRepository.eliminarRolPermiso(id_rol_permiso);
+    }
+    //guardar
+    public RolPermiso saveRolPermiso(RolPermiso rolPermiso){
+        return rolPermisoRepository.guardarRolPermiso(rolPermiso);
+    }
+    //modificar
+    public RolPermiso updateRolPermiso(RolPermiso rolPermiso){
+        return rolPermisoRepository.modificarRolPermiso(rolPermiso);
+    }
+
 
 }
