b2347c66c3df0c11695c3c91d2b5190a3beeb0f3
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:37:33 -0400

    serviceTienda

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/TiendaService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/TiendaService.java
@@ -1,5 +1,36 @@
 package com.example.EcoMarketSPA.service;
 
+import java.security.PrivateKey;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.Tienda;
+import com.example.EcoMarketSPA.repository.TiendaRepository;
+
 public class TiendaService {
+    @Autowired
+    private TiendaRepository tiendaRepository;
+    //obtener
+    public List<Tienda> getTiendas(){
+        return tiendaRepository.obtenerTiendas();
+    }
+    //bucar
+    public Tienda getTienda(int id_tienda){
+        return tiendaRepository.buscarTienda(id_tienda);
+    }
+    //eliminar
+    public int deleteTienda(int id_tienda){
+        return tiendaRepository.eliminarTienda(id_tienda);
+    }
+    //buardar
+    public Tienda saveTienda(Tienda tienda){
+        return tiendaRepository.guardarTienda(tienda);
+    }
+    //modificar
+    public Tienda updateTienda(Tienda tienda){
+        return tiendaRepository.modificarTienda(tienda);
+    }
+
 
 }
