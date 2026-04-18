6d8962173e5dee0fb2d3c703e2f52b1498680871
belenvelasco <138613417+belenvelasco@users.noreply.github.com>
2026-04-17 20:16:15 -0400

    serviceRegion

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RegionService.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/service/RegionService.java
@@ -1,5 +1,37 @@
 package com.example.EcoMarketSPA.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.EcoMarketSPA.model.Region;
+import com.example.EcoMarketSPA.repository.RegionRepository;
+
 public class RegionService {
+    @Autowired
+    private RegionRepository regionRepository;
+
+    //obtener
+    public List<Region> getRegiones(){
+        return regionRepository.obtenerRegiones();
+    }
+    //bucar
+    public Region getRegion(int id_region){
+        return regionRepository.buscarRegion(id_region);
+    }
+    //eliminar
+    public int deleteRegion(int id_region){
+        return regionRepository.eliminarRegion(id_region);
+    }
+    //guardar
+    public Region saveRegion(Region region){
+        return regionRepository.guardarRegion(region);
+    }
+    //modificar
+    public Region updateRegion(Region region){
+        return regionRepository.modificarRegion(region);
+    }
+
+
 
 }
