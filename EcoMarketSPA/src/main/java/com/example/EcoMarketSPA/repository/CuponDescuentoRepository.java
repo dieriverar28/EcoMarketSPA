9be1303fffb6dd8fc47411ffe814c491cdbdfbd6
diegoriverar28 <die.riverar@duocuc.cl>
2026-04-23 12:49:52 -0400

    cupondescuentolisto

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/repository/CuponDescuentoRepository.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/repository/CuponDescuentoRepository.java
@@ -3,65 +3,22 @@ package com.example.EcoMarketSPA.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 import com.example.EcoMarketSPA.model.CuponDescuento;
 
 @Repository
-public class CuponDescuentoRepository {
-    //Lista para guardar Cupon
-    private List<CuponDescuento>cupones=new ArrayList<>();
+public interface CuponDescuentoRepository extends JpaRepository<CuponDescuento, Integer> {
+    
+    @Query("SELECT c FROM CuponDescuento c")
+    List<CuponDescuento> obtenerCuponDescuentos();
 
-    //OBTNENER TODOS
-    public List<CuponDescuento> obtenerCuponDescuentos(){
-        return cupones;
-    }
+    @Query("SELECT c FROM CuponDescuento c WHERE c.id_cupon_descuento = :id_cupon_descuento")
+    CuponDescuento buscarCuponDescuento(int id_cupon_descuento);
 
-    //BUSCAR X id_comuna
-    public CuponDescuento buscarCuponDescuento(int id_cupon){
-        for (CuponDescuento cu : cupones) {
-            if (cu.getId_cupon() == id_cupon) {
-                return cu;
-            }
-        }
-        return null;
-    }
-
-    // ELIMINAR POR ID
-    public int eliminarCuponDescuento(int id_cupon) {
-        for (int i = 0; i < cupones.size(); i++) {
-            if (cupones.get(i).getId_cupon() == id_cupon) {
-                cupones.remove(i);
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-    // GUARDAR Cupon
-    public CuponDescuento guardarCuponDescuento(CuponDescuento cupon) {
-        cupones.add(cupon);
-        return cupon;
-    }
-
-    // MODIFICAR COMUNA
-    public int modificarCuponDescuento(CuponDescuento cupon) {
-    try {
-        for (int i = 0; i < cupones.size(); i++) {
-            if (cupones.get(i).getId_cupon() == cupon.getId_cupon()) {
-                cupones.get(i).setCodigo(cupon.getCodigo());
-                cupones.get(i).setDescuento_pct(cupon.getDescuento_pct());
-                cupones.get(i).setDescuento_monto(cupon.getDescuento_monto());
-                cupones.get(i).setFecha_expiracion(cupon.getFecha_expiracion());
-                cupones.get(i).setActivo(false);
-                return 1; // modificado
-            }
-        }
-        return 0; // no encontrado
-    } catch (Exception e) {
-        return -1; // error
-    }
-    }
+    
 
 }
