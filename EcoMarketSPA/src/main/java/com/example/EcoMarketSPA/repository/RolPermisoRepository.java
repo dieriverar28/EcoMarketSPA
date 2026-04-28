8e41fbb56ca64d963a69dd8c0646db4d77d8ce96
diegoriverar28 <die.riverar@duocuc.cl>
2026-04-28 12:01:18 -0400

    imports

--- EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/repository/RolPermisoRepository.java
+++ EcoMarketSPA/src/main/java/com/example/EcoMarketSPA/repository/RolPermisoRepository.java
@@ -3,6 +3,8 @@ package com.example.EcoMarketSPA.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.EcoMarketSPA.model.RolPermiso;
